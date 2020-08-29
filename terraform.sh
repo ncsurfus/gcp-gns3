f992d51c9061f29981e3373b66fb7c0be768df4b
Nathan Surfus <ncsurfus@gmail.com>
2020-08-29 10:39:27 -0500

    Formatting

--- terraform.sh
+++ terraform.sh
@@ -7,7 +7,8 @@ source ./secrets-macos.sh
 export TF_VAR_full_access="$(curl -s ifconfig.me)/32"
 export TF_VAR_ssh_username=$USER
 export TF_VAR_gcloud_region="us-central1-a"
-export TF_VAR_gcloud_disk_selfLink="https://www.googleapis.com/compute/v1/projects/$TF_VAR_gcloud_project/zones/$TF_VAR_gcloud_region/disks/gns3-data"
+export TF_VAR_gcloud_disk_selfLink="https://www.googleapis.com/compute/v1/projects/\
+$TF_VAR_gcloud_project/zones/$TF_VAR_gcloud_region/disks/gns3-data"
 
 # Run terraform
 cd infrastructure
